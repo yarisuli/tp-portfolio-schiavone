a786eb766abde292eb74d264038921097c17208d
Iara Genco <148562678+yarisuli@users.noreply.github.com>
2025-11-27 09:58:20 -0300

    portfolio DONE

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,6 +3,8 @@ import './Portfolio.css'
 function Portfolio() {
   return (
     <section className="portfolio-section" id="portfolio">
+      <h2 className="portfolio-title">PORTFOLIO</h2>
+      <img src="/galeria.png" alt="Galería" className="portfolio-image" />
     </section>
   )
 }
